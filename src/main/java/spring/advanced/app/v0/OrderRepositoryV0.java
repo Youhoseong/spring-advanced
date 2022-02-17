2cea561c7829ba4a9413ed2b77bdaef42454229b
Youhoseong <h0song@naver.com>
2022-02-17 22:25:40 +0900

    feat: init project

---
+++ src/main/java/spring/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,9 @@
+package spring.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+}
