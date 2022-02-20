2332dab64ff3475252fdee37e1e99b6fff89551d
Youhoseong <h0song@naver.com>
2022-02-20 16:47:39 +0900

    뭐지

---
+++ advanced/src/main/java/spring/advanced/trace/TraceStatus.java
@@ -0,0 +1,14 @@
+package spring.advanced.trace;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TraceStatus {
+    private TraceId traceId;
+    private Long startTimeMs;
+    private String message;
+
+
+}
