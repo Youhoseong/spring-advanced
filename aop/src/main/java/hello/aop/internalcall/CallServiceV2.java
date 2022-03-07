383e1ab058690f778c47c666bd6cb83ccaceca9d
Youhoseong <h0song@naver.com>
2022-03-07 13:13:55 +0900

    AOP 사용시 문제점 - 내부 호출

---
+++ aop/src/main/java/hello/aop/internalcall/CallServiceV2.java
@@ -0,0 +1,26 @@
+package hello.aop.internalcall;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class CallServiceV2 {
+
+    private final ObjectProvider<CallServiceV2> callServiceProvider;
+
+    public CallServiceV2(ObjectProvider<CallServiceV2> callServiceProvider) {
+        this.callServiceProvider = callServiceProvider;
+    }
+
+    public void external() {
+        log.info("call external");
+        CallServiceV2 proxyService = callServiceProvider.getObject();
+        proxyService.internal(); // 지연 호출
+    }
+
+    public void internal() {
+        log.info("call internal");
+    }
+}
